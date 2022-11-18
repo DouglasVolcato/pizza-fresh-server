1db882b19f11dba2550297a38321a141f0db9bdb
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-17 23:52:33 -0300

    docs: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('pizza-fresh-server')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('table')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,3 +1,12 @@
+import { IsNumber, IsPositive } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTableDto {
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Represents the table number.',
+    example: 1,
+  })
   number: number;
 }
