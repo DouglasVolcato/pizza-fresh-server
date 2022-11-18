71b4a6ed1a3aef5eb75e8b4f8223e7c3a61d3162
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-17 23:10:55 -0300

    feat: add table module

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,3 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,24 +7,11 @@ const makeSut = () => {
 };
 
 describe('AppController', () => {
-  let appController: AppController;
-
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
-
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('AppController', () => {
-    it('getAppStatus should return a message', () => {
-      const sut = makeSut();
-      const message = sut.getAppStatus();
-      expect(message).toBe(
-        'Server is running! Please check http://localhost:3333/api for swagger docs',
-      );
-    });
+  it('getAppStatus should return a message', () => {
+    const sut = makeSut();
+    const message = sut.getAppStatus();
+    expect(message).toBe(
+      'Server is running! Please check http://localhost:3333/api for swagger docs',
+    );
   });
 });
